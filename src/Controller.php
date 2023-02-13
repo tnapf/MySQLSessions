e559118ba3116386a1fcc5f994afb3dfa17b53b1
commandstring <bmxmikey2@gmail.com>
2023-02-12 19:47:25 -0500

    init

---
+++ src/Controller.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Tnapf\MysqlSessions;
+
+use CommandString\Utils\GeneratorUtils;
+use PDO;
+use stdClass;
+use Tnapf\Sessions\Exceptions\SessionDoesNotExist;
+use Tnapf\SessionsInterfaces\Controller as ControllerInterface;
+use Tnapf\SessionsInterfaces\Session as SessionInterface;
+
+class Controller implements ControllerInterface {
+    /**
+     * @var stdClass
+     */
+    private stdClass $sessions;
+
+    public function __construct(private PDO $driver) {
+        $this->sessions = new stdClass;
+
+        $this->sessions->existing = $this->sessions->new = [];
+    }
+
+    /**
+     * @param integer|null $expiresTimestamp a timestamp (in seconds) of when the session should expire; Default: 1 week
+     * @return Session
+     */
+    public function create(?int $expiresTimestamp = null): Session
+    {
+        while (!isset($id)) {
+            $id = GeneratorUtils::uuid();
+            
+            $stmt = $this->driver->prepare("SELECT id FROM sessions WHERE id = :id");
+            $stmt->execute(["id" => $id]);
+
+            if ($stmt->rowCount()) {
+                unset($id);
+            }
+        }
+
+        $expires = $expiresTimestamp ?? time() + 604800;
+
+        $session = new Session($id, $expires);
+
+        $this->sessions->new[] = &$session;
+
+        return $session;
+    }
+
+    public function get(string $id): Session
+    {        
+        $id = self::filterId($id);
+
+        if (isset($this->sessions->existing[$id]) || isset($this->sessions->new[$id])) {
+            return $this->sessions->existing[$id] ?? $this->sessions->new[$id];
+        }
+
+        $stmt = $this->driver->prepare("SELECT * FROM sessions WHERE id = :id");
+        $stmt->execute(["id" => $id]);
+
+        if (!$stmt->rowCount()) {
+            throw new SessionDoesNotExist($id);
+        }
+
+        $row = $stmt->fetch(PDO::FETCH_OBJ);
+
+        if ($row->expires < time()) {
+            $this->delete($id);
+            throw new SessionDoesNotExist($id);
+        }
+
+        $session = new Session($id, $row->expires, json_decode($row->data));
+
+        $this->sessions->existing[] = &$session;
+
+        return $session;
+    }
+
+    public function delete(SessionInterface|string $session): void
+    {
+        if ($session instanceof SessionInterface) {
+            $session = $session->id;
+        }
+
+        $stmt = $this->driver->prepare("DELETE FROM sessions WHERE id = :id");
+        $stmt->execute(["id" => $session]);
+    }
+    
+    public function __destruct() {
+        foreach ($this->sessions->new as $session) {
+            $stmt = $this->driver->prepare("INSERT INTO sessions (id, data, expires) VALUES (:id, :data, :expires)");
+
+            $stmt->execute([
+                "id" => $session->id,
+                "data" => (string)$session,
+                "expires" => $session->expires
+            ]);
+        }
+
+        foreach ($this->sessions->existing as $session) {
+            $stmt = $this->driver->prepare("UPDATE sessions SET data = :data, expires = :expires WHERE id = :id");
+
+            $stmt->execute([
+                "id" => $session->id,
+                "data" => (string)$session,
+                "expires" => $session->expires
+            ]);
+        }
+    }
+
+    private static function filterId($id): string
+    {
+        return preg_replace("/[^a-zA-Z0-9]/", "", $id);
+    }
+}
---
+++ src/Session.php
@@ -0,0 +1,82 @@
+<?php
+
+
+namespace Tnapf\MysqlSessions;
+
+use DateTime;
+use stdClass;
+use Tnapf\SessionsInterfaces\Session as SessionInterface;
+
+class Session extends SessionInterface {
+    private stdClass $data;
+
+    public function __construct(public readonly string $id, public readonly int $expires, ?stdClass $data = null) {
+        $this->data = $data ?? new stdClass;
+    }
+
+    public function set(string $name, mixed $value): self
+    {
+        $this->data->$name = $value;
+
+        return $this;
+    }
+
+    public function setCookieHeader(string $path = "/", string $domain = "", bool $secure = false, bool $httponly = false): string
+    {
+        $expiresFormat = (new DateTime())->setTimestamp($this->expires)->format("D, j n Y G:i:s");
+
+        $cookieHeader = sprintf('%s=%s; Expires=%s', "session_id", $this->id, $expiresFormat);
+
+        $empty = ["Path", "Domain"];
+
+        foreach ($empty as $key) {
+            $keyValue = ${strtolower($key)};
+            if (!empty($keyValue)) {
+                $cookieHeader .= sprintf('; %s=%s', $key, $keyValue);
+            }
+        }
+
+        $true = ["Secure", "HttpOnly"];
+
+        foreach ($true as $key) {
+            $keyValue = ${strtolower($key)};
+            if ($keyValue) {
+                $cookieHeader .= sprintf('; %s', $key);
+            }
+        }
+
+        return $cookieHeader;
+    }
+
+    public function unset(string $name, string ...$names): self
+    {
+        foreach ([$name, ...$names] as $name) {
+            unset($this->data->$name);
+        } 
+
+        return $this;
+    }
+
+    public function get(string $name): mixed
+    {
+        return $this->data->$name ?? null;
+    }
+
+    public function __set(string $name, mixed $value) {
+        $this->set($name, $value);
+    }
+
+    public function __get(string $name): mixed
+    {
+        return $this->get($name);
+    }
+
+    public function __unset(string $name): void
+    {
+        $this->unset($name);
+    }
+
+    public function __toString() {
+        return json_encode($this->data);
+    }
+}
